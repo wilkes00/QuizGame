7bf644048394166e736e920fe46b7b608d488418
Oman León <oleonaguilar@gmail.com>
2026-04-26 13:39:15 -0600

    Revert "Funcionamiento de la  carga de respuestas"

--- QuizGame/Modelos/Respuesta.cs
+++ QuizGame/Modelos/Respuesta.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +8,10 @@ namespace QuizGame.Modelos
 {
     public class Respuesta
     {
-        [JsonProperty("id_respuesta")]
         public int idRespuesta { get; set; }
-
-        [JsonProperty("texto_respuesta")]
+        public int idPregunta { get; set; } // llave foranea
         public string textoRespuesta { get; set; }
-
-        [JsonProperty("ruta_imagen")]
         public string rutaImagen { get; set; }
-
-        [JsonProperty("es_correcta")]
         public bool esCorrecta { get; set; }
     }
 }
